01f74468c00ab5730fe87bad67eb98c2cd5d43b1
ANKUR RAWAT <ankur.rawatking43@gmail.com>
2023-02-20 22:47:15 +0530

    tailwind intigrated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container bg-blue-500 text-center p-5">
       App Component
     </div>
   );
