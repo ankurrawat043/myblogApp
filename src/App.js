e88eb66c62b6524ffc4dbc3e047129095bc06d75
ANKUR RAWAT <ankur.rawatking43@gmail.com>
2023-02-20 23:08:41 +0530

    basic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Home from "./pages/Home"
 import './App.css';
 
 function App() {
   return (
-    <div className="container bg-blue-500 text-center p-5">
-      App Component
+    <div className="max-w-screen-md mx-auto pt-20">
+      <Home/>
     </div>
   );
 }
